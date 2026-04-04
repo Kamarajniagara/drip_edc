212082eda2b9986a9daa34e403e5ea41098fd31d
Kamarajniagara <niagara.kamaraj@gmail.com>
2026-04-04 12:44:47 +0530

    new changes

--- lib/views/customer/widgets/channel_widget.dart
+++ lib/views/customer/widgets/channel_widget.dart
@@ -7,7 +7,193 @@ import '../../../StateManagement/duration_notifier.dart';
 import '../../../StateManagement/mqtt_payload_provider.dart';
 import '../../../utils/constants.dart';
 
+class ChannelWidget extends StatelessWidget {
+  final Channel channel;
+  final int cIndex, channelLength;
+  final List<Agitator> agitator;
+  final String siteSno;
+  final bool isMobile;
 
+  const ChannelWidget({
+    super.key,
+    required this.channel,
+    required this.cIndex,
+    required this.channelLength,
+    required this.agitator,
+    required this.siteSno,
+    required this.isMobile
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Selector<MqttPayloadProvider, Tuple2<String?, String?>>(
+      selector: (_, provider) => Tuple2(
+        provider.getChannelOnOffStatus(channel.sNo.toString()),
+        provider.getChannelOtherData(channel.sNo.toString()),
+      ),
+      builder: (_, data, __) {
+        final status = data.item1;
+        final other = data.item2;
+
+        final statusParts = status?.split(',') ?? [];
+        if (statusParts.length > 1) {
+          channel.status = int.tryParse(statusParts[1]) ?? 0;
+        }
+
+        final otherParts = other?.split(',') ?? [];
+        if (otherParts.isNotEmpty) {
+          channel.frtMethod = otherParts[1];
+          channel.duration = otherParts[2];
+          channel.completedDrQ = otherParts[3];
+          channel.onTime = otherParts[4];
+          channel.offTime = otherParts[5];
+          channel.flowRateLpH = otherParts[6];
+        }
+
+
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              width: 70,
+              height: 120,
+              child: Stack(
+                children: [
+                  ChangeNotifierProvider(
+                    create: (_) => IncreaseDurationNotifier(
+                      channel.duration,
+                      channel.completedDrQ,
+                      double.parse(channel.flowRateLpH),
+                      onTime: int.tryParse(channel.onTime) ?? 0,
+                      offTime: int.tryParse(channel.offTime) ?? 0,
+                      initialStatus: channel.status,
+                    ),
+                    child: Consumer<IncreaseDurationNotifier>(
+                      builder: (context, durationNotifier, _) {
+                        // Use timer status for icon
+                        return Image.asset(AppConstants.getFertilizerChannelImage(
+                            cIndex,
+                            durationNotifier.currentStatus,
+                            channelLength,
+                            agitator,
+                            isMobile
+                        ));
+                      },
+                    ),
+                  ),
+                  Positioned(
+                    top: 52,
+                    left: 6,
+                    child: CircleAvatar(
+                      radius: 8,
+                      backgroundColor: Colors.teal.shade100,
+                      child: Text('${cIndex+1}',
+                        style: const TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 50,
+                    left: 18,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.circular(3),
+                      ),
+                      width: 60,
+                      child: Center(
+                        child: Text(channel.duration,
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 10,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 65,
+                    left: 18,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.circular(3),
+                      ),
+                      width: 60,
+                      child: Center(
+                        child: Text('${channel.flowRateLpH}-lph',
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 9,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  // Show timer status indicator
+                  channel.status == 1 && channel.completedDrQ !='00:00:00' ?
+                  Positioned(
+                    top: 97,
+                    left: 0,
+                    child: Container(
+                      width: 55,
+                      decoration: BoxDecoration(
+                        color: Colors.greenAccent,
+                        borderRadius: const BorderRadius.all(Radius.circular(2)),
+                        border: Border.all(color: Colors.grey, width: .50,),
+                      ),
+                      child: ChangeNotifierProvider(
+                        create: (_) => IncreaseDurationNotifier(
+                          channel.duration,
+                          channel.completedDrQ,
+                          double.parse(channel.flowRateLpH),
+                          onTime: int.tryParse(channel.onTime) ?? 0,
+                          offTime: int.tryParse(channel.offTime) ?? 0,
+                          initialStatus: channel.status,
+                        ),
+                        child: Consumer<IncreaseDurationNotifier>(
+                          builder: (context, durationNotifier, _) {
+                            // Original display
+                            return Center(
+                              child: Text(
+                                channel.frtMethod=='1' || channel.frtMethod=='3'?
+                                durationNotifier.onCompletedDrQ :
+                                '${durationNotifier.onCompletedDrQ} L',
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ):
+                  const SizedBox(),
+                ],
+              ),
+            ),
+
+            if(!isMobile)...[
+              if(kIsWeb)...[
+                const SizedBox(height: 4),
+                Container(width: 70, height: 1, color: Colors.grey.shade300),
+                const SizedBox(height: 3.5),
+                Container(width: 70, height: 1, color: Colors.grey.shade300),
+              ]
+            ]
+          ],
+        );
+      },
+    );
+  }
+}
+
+/*
 class ChannelWidget extends StatelessWidget {
   final Channel channel;
   final int cIndex, channelLength;
@@ -29,6 +215,9 @@ class ChannelWidget extends StatelessWidget {
       builder: (_, data, __) {
         final status = data.item1;
         final other = data.item2;
+
+        print("status:$status");
+        print("other:$other");
 
         final statusParts = status?.split(',') ?? [];
         if (statusParts.length > 1) {
@@ -114,7 +303,8 @@ class ChannelWidget extends StatelessWidget {
                         border: Border.all(color: Colors.grey, width: .50,),
                       ),
                       child: ChangeNotifierProvider(
-                        create: (_) => IncreaseDurationNotifier(channel.duration, channel.completedDrQ, double.parse(channel.flowRateLpH)),
+                        create: (_) => IncreaseDurationNotifier(channel.duration,
+                            channel.completedDrQ, double.parse(channel.flowRateLpH)),
                         child: Stack(
                           children: [
                             Consumer<IncreaseDurationNotifier>(
@@ -155,4 +345,4 @@ class ChannelWidget extends StatelessWidget {
       },
     );
   }
-}
+}*/
